d98cb60ab990dd44c284c1c6425524174c4da5f4
laonstory <apps@laonstory.com>
2020-09-27 22:32:12 +0900

    mongoose

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,8 +1,24 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
+import dotenv from 'dotenv';
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
 
-const api = require('./api');
+import api from './api/index.js';
+
+dotenv.config();
+
+// 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+    .then(() => {
+      console.log('Connect to MongoDB');
+    })
+    .catch(e => {
+      console.error(e);
+    });
 
 const app = new Koa();
 const router = new Router();
@@ -16,6 +32,8 @@ app.use(bodyParser());
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('Listening to port 4000');
+// PORT가 지정되어 있지 않다면 4000을 사용
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('Listening to port %d', port);
 });
