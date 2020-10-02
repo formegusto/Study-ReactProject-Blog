c5f3233b123ebcbf35465b1c86904b61c6a31bf5
formegusto <formegusto@gmail.com>
2020-10-02 20:49:22 +0900

    front Board

---
+++ blog/blog-frontend/src/containers/posts/PaginationContainer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import qs from 'qs';
+import { useSelector } from 'react-redux';
+import Pagination from '../../components/posts/Pagination';
+import { withRouter } from 'react-router-dom';
+
+const PaginationContainer = ({ location, match }) => {
+    const { lastPage, posts, loading } = useSelector(({ posts, loading }) => ({
+        lastPage: posts.lastPage,
+        posts: posts.posts,
+        loading: loading['posts/LIST_POSTS'],
+    }));
+
+    if(!posts || loading) return null;
+
+    // page가 없으면 1을 기본값으로 사용
+    const { username } = match.params;
+    const { tag, page = 1 } = qs.parse(location.search, {
+        ignoreQueryPrefix: true,
+    });
+
+    return (
+        <Pagination
+            tag={tag}
+            username={username}
+            page={parseInt(page,10)}
+            lastPage={lastPage}
+        />
+    );
+};
+
+export default withRouter(PaginationContainer);
